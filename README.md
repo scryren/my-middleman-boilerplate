0c2fdfb60b167ef574d3bec1b26802400ab8722f
scryren <scryren@gmail.com>
2017-02-26 10:04:42 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# My Middleman Boilerplate Template
+
+Customized from [jawwadzafar's](https://github.com/jawwadzafar) [Middleman Boilerplate](https://github.com/jawwadzafar/middleman-boilerplate).
+
+## Middleman on windows
+
+This is the boilerplate for Middleman projects for Windows. Currently using Middleman 4.
+
+This is for people who're having difficulties while running middleman on Windows.
+
+## Local setup
+
+
+1. Make sure you've ruby 2.2.4p230 (2015-12-16 revision 53155) or above installed.
+
+2. Install [Bundler](https://rubygems.org/gems/bundler) and [Middleman](https://rubygems.org/gems/middleman)
+
+3. Clone this repository and rename the folder with your project name.
+
+4. `cd` to your project and from there run ``bundle``
+
+5. lastly run ``bundle exec middleman``
+
+
+
+
+#### Godspeed
---
+++ config.rb
@@ -0,0 +1,59 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+
+set :css_dir, 'css'
+set :js_dir, 'js'
+set :images_dir, 'images'
+
+
+# automagically add alt tags to images
+activate :automatic_alt_tags
+
+# pretty urls
+#activate :directory_indexes
+
+# Use relative URLs
+ activate :relative_assets
+
+# Reload the browser automatically whenever files change
+configure :development do
+ activate :livereload, livereload_css_target: 'css/*', livereload_css_pattern: Regexp.new('_.*\.scss')
+end
+
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+end
